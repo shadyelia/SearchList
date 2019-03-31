dd00253a05ad7e11690055764e4dd9401795ff95
shadyelia <shady.elia@orca-tech.net>
2019-03-31 17:16:56 +0200

    add required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,24 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
+import { AddTicketComponent } from './components/add-ticket/add-ticket.component';
+import { SearchListComponent } from './components/search-list/search-list.component';
+import { AppRoutingModule } from './shared/routing/app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTicketComponent,
+    SearchListComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebaseConfig, 'SearchList'),
     AngularFirestoreModule, // Only required for database features
     AngularFireAuthModule, // Only required for auth features,
-    AngularFireStorageModule // Only required for storage features
+    AngularFireStorageModule, // Only required for storage features
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/routing/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+// Required services for navigation
+import { Routes, RouterModule } from '@angular/router';
+
+// Import all the components for which navigation service has to be activated
+import { SearchListComponent } from '../../components/search-list/search-list.component'
+import { AddTicketComponent } from '../../components/add-ticket/add-ticket.component'
+
+const routes: Routes = [
+  { path: '', redirectTo: '/search-list', pathMatch: 'full' },
+  { path: 'search-list', component: SearchListComponent },
+  { path: 'add-ticket', component: AddTicketComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }

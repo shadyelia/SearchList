ef72ff033af8d59b789e85402f179569e24ed565
shadyelia <shady.elia@orca-tech.net>
2019-04-01 17:10:02 +0200

    push notification without premission

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 
 // Firebase modules
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
@@ -14,10 +15,23 @@ import { environment } from '../environments/environment';
 import { AddTicketComponent } from './components/add-ticket/add-ticket.component';
 import { SearchListComponent } from './components/search-list/search-list.component';
 import { AppRoutingModule } from './shared/routing/app-routing.module';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
 
 import { MatOptionModule , MatSelectModule , MatFormFieldModule} from '@angular/material';
 import { FormsModule } from '@angular/forms';
 
+import { MessagingService } from './messaging.service';
+import { AsyncPipe } from '../../node_modules/@angular/common';
+
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyC8Acx2PrlGG32AM5pEairi1v-TTQ0owJA',
+  authDomain: 'searchlist-d4c0e.firebaseapp.com',
+  databaseURL: 'https://searchlist-d4c0e.firebaseio.com',
+  projectId: 'searchlist-d4c0e',
+  storageBucket: 'searchlist-d4c0e.appspot.com',
+  messagingSenderId: '180307103178'
+  };
 
 @NgModule({
   declarations: [
@@ -28,17 +42,19 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig, 'SearchList'),
+    AngularFireModule.initializeApp(firebaseConfig),
     AngularFirestoreModule, // Only required for database features
     AngularFireAuthModule, // Only required for auth features,
     AngularFireStorageModule, // Only required for storage features
+    AngularFireDatabaseModule,
+    AngularFireMessagingModule,
     AppRoutingModule,
     MatOptionModule,
     MatSelectModule,
     MatFormFieldModule,
     FormsModule
   ],
-  providers: [],
+  providers: [MessagingService, AsyncPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
